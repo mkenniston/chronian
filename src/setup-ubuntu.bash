d78e50ceb0c8e47275f5c7107c1d2552411b3d0f
Michael S. Kenmniston <mike@chronian.com>
2019-10-28 10:42:30 -0700

    cleaned up and tested setup script for ubuntu

--- src/setup-ubuntu.bash
+++ src/setup-ubuntu.bash
@@ -5,10 +5,25 @@
 # supported languages, by installing all the packages needed.
 # You will need to run this with "sudo".
 
+apt-get update
+apt-get install mit-scheme  # needed to compile lexical scanners
 apt-get install python
-apt-get install javacc
-apt-get install java-sdk
-apt-get install node
-apt-get install gcc
-apt-get install flake8
+apt-get install flake8  # lint for python
+apt-get install openjdk-8-jdk  # includes both javac and java
+apt-get install nodejs  # javascript interpreter
+apt-get install gcc  # plain C (not C++) compiler
 
+echo
+echo "=================================="
+echo
+
+for CMD in "scheme --version" "python --version" "flake8 --version" "javac -version" "java -version" "nodejs --version" "gcc --version"
+do
+  $CMD
+  if [ $? -eq 0 ]
+  then
+    echo "OK"
+  else
+    echo "**** FAILED ****"
+  fi
+done
